5c8dab186be13908539e31f085f2aed0de1cb0bb
FazarSN <fazar.syukri@gmail.com>
2016-01-10 11:40:54 +0700

    final

---
+++ plot1.R
@@ -0,0 +1,7 @@
+setwd('/home/fazar/Downloads')
+library(sqldf)
+powerdata<-read.csv.sql("household_power_consumption.txt", sql = "select * from file where Date = '2/2/2007' or Date = '1/2/2007'", header = TRUE, sep = ";")
+closeAllConnections()
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(powerdata$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
